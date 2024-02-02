56f264deb91d9c5d51fdde25365d6d3b88814d3c
ihamza494 <ihamza494@gmail.com>
2024-02-02 20:30:56 +0500

    updated header

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,23 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import SignIn from "./pages/SignIn"
+import About from "./pages/About"
+import Home from "./pages/Home"
+import SignUp from "./pages/SignUp"
+import Dashboard from "./pages/Dashboard"
+import Projects from "./pages/Projects"
+import Header from "../components/Header"
 export default function App() {
   return (
-    <h1 className="text-3xl font-bold underline text-red-400">
-      Hello world!
-    </h1>
+    <BrowserRouter>
+    <Header/>
+      <Routes>
+        <Route path="/" element={<Home/>}/>
+        <Route path="/about" element={<About/>}/>
+        <Route path="/sign-in" element={<SignIn/>}/>
+        <Route path="/sign-up" element={<SignUp/>}/>
+        <Route path="/dashboard" element={<Dashboard/>}/>
+        <Route path="/projects" element={<Projects/>}/>
+      </Routes>
+    </BrowserRouter>
   )
 }
