12fc8f2809e65b1383e7ffbc73c0f555863cabee
ihamza494 <ihamza494@gmail.com>
2024-02-11 12:58:32 +0500

    created error handler

--- api/index.js
+++ api/index.js
@@ -1,6 +1,10 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
+import userRoutes from './routes/user.route.js';
+import authRoutes from './routes/auth.route.js';
+import adminRoutes from './routes/admin.route.js';
+
 
 dotenv.config();
 
@@ -16,6 +20,23 @@ mongoose
 })
 const app = express();
 
+app.use(express.json());
+
+
 app.listen(3000, ()=>{
     console.log("Server is running on 3000...");
 })
+
+app.use('/api/user', userRoutes);
+app.use('/api/auth', authRoutes);
+app.use('/api/admin', adminRoutes);
+
+app.use((err, req, res, next) =>{
+    const statusCode = err.statusCode || 500;
+    const message = err.message || 'Internal Server Error';
+    res.status(statusCode).json({
+        success: false,
+        statusCode,
+        message,
+    });
+});
